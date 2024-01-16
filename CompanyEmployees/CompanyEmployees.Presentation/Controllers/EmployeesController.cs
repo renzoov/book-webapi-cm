3b942903142217c2041a9e93d619e2c337cfadca
Renzo Osorio <renzo.osorio1909@gmail.com>
2024-01-15 22:23:29 -0500

    agregar validaciones en entidades y controladores

--- CompanyEmployees/CompanyEmployees.Presentation/Controllers/EmployeesController.cs
+++ CompanyEmployees/CompanyEmployees.Presentation/Controllers/EmployeesController.cs
@@ -34,6 +34,8 @@ namespace CompanyEmployees.Presentation.Controllers
         {
             if (employee is null) BadRequest("EmployeeForCreationDto object is null");
 
+            if (!ModelState.IsValid) return UnprocessableEntity(ModelState);
+
             var employeeToReturn = _service.EmployeeService.CreateEmployeeForCompany(companyId, employee, trackChanges: false);
 
             return CreatedAtRoute("GetEmployeeForCompany", new { companyId, id = employeeToReturn.Id }, employeeToReturn);
@@ -52,6 +54,8 @@ namespace CompanyEmployees.Presentation.Controllers
         {
             if (employee is null) return BadRequest("EmployeeForUpdateDto object is null");
 
+            if (!ModelState.IsValid) return UnprocessableEntity(ModelState);
+
             _service.EmployeeService.UpdateEmployeeForCompany(companyId, id, employee, compTrackChanges: false, empTrackChanges: true);
 
             return NoContent();
@@ -64,7 +68,11 @@ namespace CompanyEmployees.Presentation.Controllers
 
             var result = _service.EmployeeService.GetEmployeeForPatch(companyId, id, compTrackChanges: false, empTrackChanges: true);
 
-            patchDoc.ApplyTo(result.employeeToPatch);
+            patchDoc.ApplyTo(result.employeeToPatch, ModelState);
+
+            TryValidateModel(result.employeeToPatch);
+
+            if (!ModelState.IsValid) return UnprocessableEntity(ModelState);
 
             _service.EmployeeService.SaveChangesForPatch(result.employeeToPatch, result.employeeEntity);
 
--- CompanyEmployees/Shared/DataTransferObjects/CompanyForCreationDto.cs
+++ CompanyEmployees/Shared/DataTransferObjects/CompanyForCreationDto.cs
@@ -1,4 +1,20 @@
-﻿namespace Shared.DataTransferObjects
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.DataTransferObjects
 {
-    public record CompanyForCreationDto(string Name, string Address, string Country, IEnumerable<EmployeeForCreationDto> Employees);
+    public record CompanyForCreationDto 
+    {
+        [Required(ErrorMessage = "Company name is a required field.")]
+        [MaxLength(30, ErrorMessage = "Maximun length for the Name is 30 characters.")]
+        public string? Name { get; init; }
+
+        [Required(ErrorMessage = "Address is a required field.")]
+        [MaxLength(30, ErrorMessage = "Maximun length for the Address is 30 characters.")]
+        public string? Address { get; init; }
+
+        [MaxLength(20, ErrorMessage = "Maximun length for the Country is 30 characters.")]
+        public string? Country { get; init; }
+
+        public IEnumerable<EmployeeForCreationDto>? Employees { get; init; }
+    }
 }
