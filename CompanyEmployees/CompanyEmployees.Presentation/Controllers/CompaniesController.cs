c5232dbea5606e49795059adde0f7f06a3fd52f3
Renzo Osorio <renzo.osorio1909@gmail.com>
2024-01-26 15:23:19 -0500

    agregar cabecera httpoptions y httphead

--- CompanyEmployees/CompanyEmployees.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployees/CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -68,5 +68,12 @@ namespace CompanyEmployees.Presentation.Controllers
 
             return NoContent();
         }
+
+        [HttpOptions]
+        public IActionResult GetCompaniesOptions()
+        {
+            Response.Headers.Add("Allow", "GET, OPTIONS, POST");
+            return Ok();
+        }
     }
 }
